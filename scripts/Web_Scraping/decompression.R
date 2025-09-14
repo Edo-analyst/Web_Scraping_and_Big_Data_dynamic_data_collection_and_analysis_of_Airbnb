7539df70c369056ae143ccd91668de90833060ba
Edoardo <matez4856@gmail.com>
2025-09-14 18:48:06 +0200

    Update decompression.R

--- scripts/Web_Scraping/decompression.R
+++ scripts/Web_Scraping/decompression.R
@@ -2,13 +2,10 @@ library(R.utils)
 
 decompress_and_replace_gz <- function(city_name) {
   # Path to city directory
-  directory <- file.path("C:/Users/matez/
-                         Desktop/Tesi_selenium",
-                         city_name)
+  directory <- file.path("C:/Desktop/Airbnb_Project", city_name)
   
   # List of .csv.gz files 
-  gz_files <- list.files(directory, pattern =
-                           "\\.csv\\.gz$", full.names = TRUE)
+  gz_files <- list.files(directory, pattern = "\\.csv\\.gz$", full.names = TRUE)
   
   # Loop through each .csv.gz file
   for (gz_file in gz_files) {
@@ -28,3 +25,4 @@ decompress_and_replace_gz <- function(city_name) {
   # Completion message after processing all files for the city
   cat("Decompression completed for city:", city_name, "\n")
 }
+
