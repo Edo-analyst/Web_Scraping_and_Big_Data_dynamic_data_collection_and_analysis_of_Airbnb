8fede9da582e2fe257e0f9011b82c044078e6332
Edoardo <matez4856@gmail.com>
2025-09-14 18:54:08 +0200

    Add files via upload

---
+++ scripts/Data_Cleansing/listings_aggregation.R
@@ -0,0 +1,25 @@
+
+
+data_listings <- function(city_name) {
+  # Define the directory path
+  path <- paste0("C:/Users/matez/Desktop/Pulizia_dataset/", city_name)
+
+  # Change the working directory
+  setwd(path)
+  
+  # List all .csv files in the directory
+  files_csv <- list.files(pattern = "listings_.*.csv")   
+  
+  # Import and merge all files
+  listings_list <- lapply(files_csv, function(file) {
+    read.csv(file, header = TRUE, sep = ",", quote = "\"", dec = ".", 
+             fill = TRUE, comment.char = "", na.strings="")
+  })
+  
+  # Combine all dataframes into one
+  combined_data <- do.call(rbind, listings_list)   
+  
+  return(combined_data)
+}
+
+
