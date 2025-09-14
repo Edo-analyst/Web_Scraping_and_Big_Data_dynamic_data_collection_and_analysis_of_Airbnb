dad5f29520fc79066e846fe20fb1cf8ff093f096
Edoardo <matez4856@gmail.com>
2025-09-14 17:51:25 +0200

    Update core_code.R

--- scripts/Web_Scraping/core_code.R
+++ scripts/Web_Scraping/core_code.R
@@ -2,8 +2,7 @@
 library(RSelenium)
 
 # Open Selenium
-rD <- rsDriver(browser="firefox", port=4555L, verbose=F, 
-               phantomver = NULL, chromever = NULL)
+rD <- rsDriver(browser="firefox", port=4555L, verbose=F, phantomver = NULL, chromever = NULL)
 remDr <- rD$client
 #remDr$open()
 
@@ -11,29 +10,24 @@ remDr <- rD$client
 url <- "http://insideairbnb.com/get-the-data.html"
 remDr$navigate(url)
 
-
-##Italy elements
-city_section <- remDr$findElements(
-  using = 'xpath', '//h3[contains(text(), "Italy")]')
+# Italy elements
+city_section <- remDr$findElements(using = 'xpath', '//h3[contains(text(), "Italy")]')
 
 # Extract text
-city_title <- sapply(city_section, 
-                     function(el) el$getElementText())
+city_title <- sapply(city_section, function(el) el$getElementText())
 
 # Cities names
-city_name <- sapply(city_title,
-                    function(x) strsplit(x, ",")[[1]][1])  
+city_name <- sapply(city_title, function(x) strsplit(x, ",")[[1]][1])  
 
 source("click_function.R")   
 source("download_files.R")   
 source("decompression.R")    
 
 for(i in 1 : length(city_name)){
-  button_click(city_name[i])  
-  download_files(city_name[i]) 
-  decompress_and_replace_gz(city_name[i])
+    button_click(city_name[i])  
+    download_files(city_name[i]) 
+    decompress_and_replace_gz(city_name[i])
 }
 
 # Close Selenium
 remDr$close()
-
