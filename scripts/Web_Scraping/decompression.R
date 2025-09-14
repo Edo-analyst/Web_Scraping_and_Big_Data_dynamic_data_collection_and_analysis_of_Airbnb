39e23c4681a6251138704e02b3032ae65451ff7d
Edoardo <matez4856@gmail.com>
2025-09-14 18:50:07 +0200

    Update decompression.R

--- scripts/Web_Scraping/decompression.R
+++ scripts/Web_Scraping/decompression.R
@@ -10,19 +10,17 @@ decompress_and_replace_gz <- function(city_name) {
   # Loop through each .csv.gz file
   for (gz_file in gz_files) {
     # Name of the file after decompression 
-    decompressed_file <- sub("\\.csv\\.gz$", ".csv",
-                             gz_file)
+    decompressed_file <- sub("\\.csv\\.gz$", ".csv", gz_file)
     
     # Decompress the .csv.gz file (with the .csv extension)
-    gunzip(gz_file, destname = decompressed_file,
-           overwrite = TRUE)
+    gunzip(gz_file, destname = decompressed_file, overwrite = TRUE)
     
     # Print a message for each decompressed file
-    cat("File decompressed:", gz_file, "to",
-        decompressed_file, "\n")
+    cat("File decompressed:", gz_file, "to", decompressed_file, "\n")
   }
   
   # Completion message after processing all files for the city
   cat("Decompression completed for city:", city_name, "\n")
 }
+
 
