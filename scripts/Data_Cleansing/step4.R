404bb06293653cadd02bc8f4fe32c5b00f775ca3
Edoardo <matez4856@gmail.com>
2025-09-15 18:12:28 +0200

    Update step4.R

--- scripts/Data_Cleansing/step4.R
+++ scripts/Data_Cleansing/step4.R
@@ -1,47 +1,37 @@
+dati_def$property_type=NULL   # Drop original property_type (only keep recoded version)
 
-dati_def$property_type=NULL
-
-# Funzione per estrarre i nomi delle variabili di tipo fattore
+# Function to extract the names of factor variables
 fattori <- function(dataset) {
-  # Filtra le colonne che sono di tipo factor
+  # Select columns that are factors
   nomi_fattori <- names(dataset)[sapply(dataset, is.factor)]
   return(nomi_fattori)
 }
 
-# host_response_time
-sort(table(dati_def$host_response_time), 
-     decreasing = TRUE)/nrow(dati_def)
+# Host response time: check distribution (as proportion of dataset)
+sort(table(dati_def$host_response_time), decreasing = TRUE)/nrow(dati_def)
+
+# Recode response time: merge "within a day" and "a few days or more" into "more time"
 levels(dati_def$host_response_time)[levels(
-  dati_def$host_response_time) %in% c("within a day"
-              , "a few days or more")] <- "more time"
+  dati_def$host_response_time) %in% c("within a day", "a few days or more")] <- "more time"
 
-# host_is_superhost
-sort(table(dati_def$host_is_superhost), 
-     decreasing = TRUE)/nrow(dati_def)
+# Host is superhost: check distribution (as proportion of dataset)
+sort(table(dati_def$host_is_superhost), decreasing = TRUE)/nrow(dati_def)
 
-# host_identity_verified
-sort(table(dati_def$host_identity_verified), 
-     decreasing = TRUE)/nrow(dati_def)
+# Host identity verified: check distribution (as proportion of dataset)
+sort(table(dati_def$host_identity_verified), decreasing = TRUE)/nrow(dati_def)
 
-# property_type
-sort(table(dati_def$property_type_recoded), 
-     decreasing = TRUE)/nrow(dati_def)
+# Property type (recoded): check distribution (as proportion of dataset)
+sort(table(dati_def$property_type_recoded), decreasing = TRUE)/nrow(dati_def)
 
-# room_type
-sort(table(dati_def$room_type), 
-     decreasing = TRUE)/nrow(dati_def)
-levels(dati_def$room_type)[levels(dati_def$room_type) 
-      %in% c("Hotel room", "Private room", 
-             "Shared room")] <- "Private/Shared/Hotel"
+# Room type: check distribution and merge categories
+sort(table(dati_def$room_type), decreasing = TRUE)/nrow(dati_def)
 
+# Recode room_type: merge "Hotel room", "Private room", "Shared room" into "Private/Shared/Hotel"
+levels(dati_def$room_type)[levels(dati_def$room_type) %in% 
+      c("Hotel room", "Private room", "Shared room")] <- "Private/Shared/Hotel"
 
-# instant_bookable
-sort(table(dati_def$instant_bookable), 
-     decreasing = TRUE)/nrow(dati_def)
+# Instant bookable: check distribution (as proportion of dataset)
+sort(table(dati_def$instant_bookable), decreasing = TRUE)/nrow(dati_def)
 
-# city
-sort(table(dati_def$city), 
-     decreasing = TRUE)/nrow(dati_def)
-
-
-
+# City: check distribution (as proportion of dataset)
+sort(table(dati_def$city), decreasing = TRUE)/nrow(dati_def)
