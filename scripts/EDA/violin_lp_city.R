a80feaaad53ab68142e13b018ce188ba9c266cb6
Edoardo <matez4856@gmail.com>
2025-09-18 21:45:53 +0200

    Update violin_lp_city.R

--- scripts/EDA/violin_lp_city.R
+++ scripts/EDA/violin_lp_city.R
@@ -1,17 +1,16 @@
 library(ggplot2)
 violin_plot <- ggplot(last_quarter, aes(x = reorder(city, lp), y = lp, fill = city)) +
   geom_violin(alpha = 0.7) +
-  geom_boxplot(width = 0.1, outlier.shape = NA, color = "white") + # Facoltativo, per aggiungere un boxplot
+  geom_boxplot(width = 0.1, outlier.shape = NA, color = "white") + # Optional, to add a boxplot
   labs(
     x = NULL,
-    y = "Log Prezzo"
+    y = "Log Price"
   ) +
   theme_minimal() +
   theme(
-    axis.text.x = element_text(size = 8, angle = 0, hjust = 0.5), # Riduci la dimensione del testo delle città e mettile orizzontali
-    axis.title.x = element_text(size = 10), # Dimensione dell'etichetta asse x
-    axis.title.y = element_text(size = 10), # Dimensione dell'etichetta asse y
-    plot.title = element_blank(), # Rimuove il titolo
-    legend.position = "none" # Rimuove la legenda
+    axis.text.x = element_text(size = 8, angle = 0, hjust = 0.5), # Reduce city text size and make horizontal
+    axis.title.x = element_text(size = 10), # X-axis label size
+    axis.title.y = element_text(size = 10), # Y-axis label size
+    plot.title = element_blank(), # Remove title
+    legend.position = "none" # Remove legend
   )
-
